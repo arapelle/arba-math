c6bca3eeee162d155e31cf23f5c1574a9b7019eb
Aymeric Pellé <aympelle@gmail.com>
2024-01-17 01:00:41 +0100

    v0.5.0: Start version.

--- test/project_version_tests.cpp
+++ test/project_version_tests.cpp
@@ -1,11 +1,11 @@
 #include <arba/math/version.hpp>
-#include <gtest/gtest.h>
 #include <cstdlib>
+#include <gtest/gtest.h>
 
 TEST(project_version_tests, test_version_macros)
 {
     ASSERT_EQ(ARBA_MATH_VERSION_MAJOR, 0);
-    ASSERT_EQ(ARBA_MATH_VERSION_MINOR, 4);
+    ASSERT_EQ(ARBA_MATH_VERSION_MINOR, 5);
     ASSERT_EQ(ARBA_MATH_VERSION_PATCH, 0);
-    ASSERT_STREQ(ARBA_MATH_VERSION, "0.4.0");
+    ASSERT_STREQ(ARBA_MATH_VERSION, "0.5.0");
 }
