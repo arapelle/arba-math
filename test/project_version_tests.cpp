613c5866a284797cf5782ef0ffdd99c27b0412be
Aymeric Pellé <aympelle@gmail.com>
2025-02-13 01:14:02 +0100

    v0.6.0: Start dev version.

--- test/project_version_tests.cpp
+++ test/project_version_tests.cpp
@@ -4,7 +4,7 @@
 TEST(project_version_tests, test_version_math)
 {
     constexpr unsigned major = 0;
-    constexpr unsigned minor = 5;
+    constexpr unsigned minor = 6;
     constexpr unsigned patch = 0;
     static_assert(arba::math::version.core() == arba::vrsn::tri_version(major, minor, patch));
 }
